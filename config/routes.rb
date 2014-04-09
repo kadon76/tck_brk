c34dc01e053e81bae13d5a1c94e5dd5f876197d4
Kadon Hodson <kkhodson@gmail.com>
2014-04-09 16:24:03 -0500

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TckBrk::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/golf"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
